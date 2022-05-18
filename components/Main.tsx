77df9d64fbdbb9a81ca796f18a04865cd34fb4eb
yonghk423 <yonghk423423@gmail.com>
2022-05-18 13:33:10 +0900

    slider

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,11 +1,12 @@
-import type { NextPage } from 'next'
+// import Head from 'next/head'
+// import Link from 'next/link';
+import dynamic from 'next/dynamic'
 import styled from "styled-components";
-import Head from 'next/head'
-import Link from 'next/link';
 import { useQuery } from 'react-query';
-import Header from './Header';
 import { getMovies, IGetMoviesResult } from '../pages/api/api';
 import { makeImagePath } from './Utils';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
   background: black;
@@ -39,9 +40,45 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+
+const rowVariants = {
+  hidden: {
+    x: 1000
+  }, 
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -1000,
+  },
+};
+
 export default function Main() {
   const {data, isLoading} = useQuery<IGetMoviesResult>(["movies", "nowPlaying"],  getMovies )
   console.log(data, isLoading);
+
+  const [index, setIndex] = useState(0)
+  const increaseIndex = () => setIndex((prev) => prev + 1);
   return (   
     <>
       <Wrapper>
@@ -49,10 +86,26 @@ export default function Main() {
           <Loader>Loading...</Loader>
     ) : ( 
       <>
-        <Banner bgPhoto={makeImagePath(data?.results[3].backdrop_path || "")}>
-          <Title>{data?.results[3].title}</Title>
-          <Overview>{data?.results[3].overview}</Overview>
+        <Banner bgPhoto={makeImagePath(data?.results[2].backdrop_path || "")}>
+          <Title>{data?.results[2].title}</Title>
+          <Overview>{data?.results[2].overview}</Overview>
         </Banner>
+        <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
       </>
       )}
       </Wrapper>
